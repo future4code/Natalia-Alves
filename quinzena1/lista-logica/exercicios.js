f9ac885fd26d14ab670ec980e65dddbadb0af603
Natalia Alves <nataliavalins@gmail.com>
2021-09-17 23:56:16 -0300

    projeto-para-concluir

---
+++ quinzena1/lista-logica/exercicios.js
@@ -0,0 +1,95 @@
+// Exemplos
+
+// Exercício 0A
+function soma() {
+  // escreva seu código aqui
+  const num1 = prompt('Digite o primeiro número')
+  const num2 = prompt('Digite o segundo número')
+
+  console.log(Number(num1) + Number(num2))
+}
+
+// Exercício 0B
+function imprimeMensagem() {
+  // escreva seu código aqui
+  const mensagem = prompt('Digite sua mensagem')
+
+  console.log(mensagem)
+}
+
+// ---------------------------------------------------
+// Exercícios
+
+// Exercício 1
+function calculaAreaRetangulo(altura, largura) {
+// escreva seu código aqui
+let alturaTexto = Number prompt ("Insira o valor da altura do retângulo")
+let larguraTexto = Number prompt ("Insira o valor da largura do retângulo")
+let areaRetangulo = altura * largura
+console.log (areaRetangulo)
+}
+
+
+
+// Exercício 2
+function imprimeIdade(idadeUsuario) {
+  // escreva seu código aqui
+  let anoAtualTexto = prompt ("Em que ano estamos?")
+  let anoNascimentoTexto = prompt ("Em que ano você nasceu?")
+  let anoAtual = Number (anoAtualTexto)
+  let anoNascimento = Number (anoNascimentoTexto)
+
+  idadeUsuario = anoAtual - anoNascimento
+
+  console.log (idadeUsuario)
+}
+
+// Exercício 3
+function calculaIMC() {
+  // escreva seu código aqui
+}
+
+// Exercício 4
+function imprimeInformacoesUsuario() {
+  // escreva seu código aqui
+}
+
+// Exercício 5
+function imprimeTresCoresFavoritas() {
+  // escreva seu código aqui
+}
+
+// Exercício 6
+function retornaStringEmMaiuscula() {
+  // escreva seu código aqui
+}
+
+// Exercício 7
+function calculaIngressosEspetaculo() {
+  // escreva seu código aqui
+}
+
+// Exercício 8
+function checaStringsMesmoTamanho() {
+  // escreva seu código aqui
+}
+
+// Exercício 9
+function checaIgualdadeDesconsiderandoCase() {
+  // escreva seu código aqui
+}
+
+// Exercício 10
+function checaRenovacaoRG() {
+  // escreva seu código aqui
+}
+
+// Exercício 11
+function checaAnoBissexto() {
+  // escreva seu código aqui
+}
+
+// Exercício 12
+function checaValidadeInscricaoLabenu() {
+  // escreva seu código aqui
+}
