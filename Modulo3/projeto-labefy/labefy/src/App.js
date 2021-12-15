ac2aed03c66fb98e9daf42fda36bf8ecba6cbc98
Natalia Alves <nataliavalins@gmail.com>
2021-12-14 23:08:50 -0300

    labefy-criacao

---
+++ Modulo3/projeto-labefy/labefy/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p>só isso aqui já deu trabalho rs</p>
+    </div>
+  );
+}
+
+export default App;
