cd5cb404f454fd954edbba58ef939a4caa85c425
Natalia Alves <nataliavalins@gmail.com>
2021-12-19 21:59:56 -0300

    campo-add-musica

--- Modulo3/projeto-labefy/labefy/src/components/CriarPlaylists/CriarPlaylists.js
+++ Modulo3/projeto-labefy/labefy/src/components/CriarPlaylists/CriarPlaylists.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import styled from "styled-components";
 import axios from "axios";
-import AdicionarMusica from "../Adicionar Musica/AdicionarMusica";
+
 
 const Input = styled.input`
 display: flex;
--- Modulo3/projeto-labefy/labefy/src/components/ExibirPlaylists/ExibirPlaylists.js
+++ Modulo3/projeto-labefy/labefy/src/components/ExibirPlaylists/ExibirPlaylists.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import styled from "styled-components";
+import AdicionarMusica from "../Adicionar Musica/AdicionarMusica";
 import axios from "axios";
 
 export default class ExibirPlaylists extends React.Component {
@@ -23,19 +24,42 @@ export default class ExibirPlaylists extends React.Component {
         .catch((error)=>console.log(error))
     }
 
+    deletarPlaylist = (id) => {
+        const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}'
+        axios.delete(url, {
+          headers: {
+            Authorization: "natalia-alves-joy"
+          }
+        })
+          .then((response) => {
+            alert("Lista deletada com sucesso!")
+            this.pegaPlaylists()
+            console.log(response)
+          })
+          .catch((error) => {
+            alert("Ocorreu um erro, tente novamente  Mais tarde")
+            console.log(error.response)
+          })
+      }
+
     render(){
 
         let mapPlaylists = this.state.playlists.map ((playlists)=>{
-            return <li><b>Nome:</b>{playlists.name}<br/>
+            return <li><b>Nome:</b>{playlists.name} <button onClick={() => this.deletarPlaylist (playlists.id)}>x</button><br/>
             <b>ID:</b>{playlists.id}
-            </li>
-            
+            <div>
+            <AdicionarMusica/>
+            </div>
+            </li>    
         })
       return (
-        <div key = {mapPlaylists.id}>
-            <h3>Número de Playlists: {mapPlaylists.length}</h3>
-            <ol>{mapPlaylists}</ol>
+        <div>
+            <div key = {mapPlaylists.id}>
+                <h3>Número de Playlists: {mapPlaylists.length}</h3>
+            <ol>{mapPlaylists} </ol>
+            </div>
         </div>
+       
       ); 
     }
     
