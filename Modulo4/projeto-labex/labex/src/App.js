9755999df3212c07e786264ec904a7c6541ce4a9
Natalia Alves <nataliavalins@gmail.com>
2022-01-18 23:11:00 -0300

    largada-labex

---
+++ Modulo4/projeto-labex/labex/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>Pedimos desculpas pelo transtorno, mas o wireframe consumiu toda nossa massa
+        cinzenta, já passa das 23h e levantamos as 5h. Nesse espaço ainda haverá um 
+        lindo aplicativo depois que essa linda cabecinha tiver um descanso. </p>
+
+        <p>até mais ver </p>
+    </div>
+  );
+}
+
+export default App;
