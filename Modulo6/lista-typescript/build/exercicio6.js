06090bd01bc77df19d681a3e930e2986cf70d8a9
Natalia Alves <nataliavalins@gmail.com>
2022-03-27 21:01:59 -0300

    exercicio-6

--- Modulo6/lista-typescript/build/exercicio6.js
+++ Modulo6/lista-typescript/build/exercicio6.js
@@ -1,1 +1,25 @@
+const listaClientes = [
+    { cliente: "João", saldoTotal: 1000, debitos: [100, 200, 300] },
+    { cliente: "Paula", saldoTotal: 7500, debitos: [200, 1040] },
+    { cliente: "Pedro", saldoTotal: 10000, debitos: [5140, 6100, 100, 2000] },
+    { cliente: "Luciano", saldoTotal: 100, debitos: [100, 200, 1700] },
+    { cliente: "Artur", saldoTotal: 1800, debitos: [200, 300] },
+    { cliente: "Soter", saldoTotal: 1200, debitos: [] }
+];
+function clientesParaEmprestimo(lista) {
+    const novaListaClientes = lista.map((cliente) => {
+        let debitos = 0;
+        for (let i = 0; i < cliente.debitos.length; i++) {
+            debitos += cliente.debitos[i];
+        }
+        cliente["saldoTotal"] -= debitos;
+        return cliente;
+    });
+    const emprestimoAprovado = novaListaClientes
+        .filter((cliente) => {
+        return cliente.saldoTotal < 0;
+    });
+    return emprestimoAprovado;
+}
+console.log(clientesParaEmprestimo(listaClientes));
 //# sourceMappingURL=exercicio6.js.map
