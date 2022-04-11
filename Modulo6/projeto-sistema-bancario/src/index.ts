4a0b59bd5fbdbf8175ed08fcca26b28650271a98
Natalia Alves <nataliavalins@gmail.com>
2022-04-10 23:10:57 -0300

    requisitos-basicos-feitos

--- Modulo6/projeto-sistema-bancario/src/index.ts
+++ Modulo6/projeto-sistema-bancario/src/index.ts
@@ -1,14 +1,41 @@
-import express, { Request, Response } from 'express';
+import express, { Request , Response } from 'express';
 import cors from 'cors';
 import { AddressInfo } from "net";
-
-
-// import { v4 as generateId } from 'uuid';
+import { clients } from './data';
+import { v4 as generateId } from 'uuid';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.post ('/clients', (req: Request, res: Response) => {
+
+  const newClientName = req.body.name
+  const newClientCPF = req.body.cpf
+  const newClientAge = req.body.age 
+
+
+  const createNewClient = {
+    id: generateId(),
+    name: newClientName,
+    cpf: newClientCPF,
+    age: newClientAge,
+  }
+
+  if (newClientAge < 18) {
+    res.status(404).send("Não é possível criar conta para menor de 18 anos")
+    return
+  }
+
+  console.log(createNewClient)
+  clients.push(createNewClient)
+  res.status(200).send("ok")
+})
+
+app.get ('/clients', (req:Request, res: Response) => {
+  res.status(200).send(clients)
+})
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
@@ -17,4 +44,4 @@ const server = app.listen(process.env.PORT || 3003, () => {
     } else {
       console.error(`Failure upon starting server.`);
     }
-  });;
+  });
