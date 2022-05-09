f9ab4096761c7dd40b5ef91ad2bb8679a16fa59a
Natalia Alves <nataliavalins@gmail.com>
2022-05-08 23:01:22 -0300

    endpoints-minimos-finalizados

--- Modulo6/LabECommerce/LabECommerce/src/app.ts
+++ Modulo6/LabECommerce/LabECommerce/src/app.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { Express } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
+import connection from "./connection";
 
 const app = express();
 
@@ -8,12 +9,12 @@ app.use(express.json());
 app.use(cors());
 
 const server = app.listen(process.env.PORT || 3003, () => {
-  if (server) {
-    const address = server.address() as AddressInfo;
-    console.log(`Server is running in http://localhost:${address.port}`);
-  } else {
-    console.error(`Failure upon starting server.`);
-  }
-});
-
-export default app
+    if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+      console.error(`Failure upon starting server.`);
+    }
+  });
+  
+  export default app
