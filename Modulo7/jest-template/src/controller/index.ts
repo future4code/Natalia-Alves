e92899c7ec8ca5021e57ecd5c40a5b4019d7db77
Natalia Alves <nataliavalins@gmail.com>
2022-06-28 23:23:43 -0300

    testes-automatizados-obrigatorios

---
+++ Modulo7/jest-template/src/controller/index.ts
@@ -0,0 +1,13 @@
+import { User } from "../model/user"
+
+export function performPurchase(user: User, value: number): User | undefined {
+	if(user.balance >= value) {
+		return {
+			...user,
+			balance: user.balance - value		
+		}
+	}
+	return undefined
+}
+
+//test/index.spec.ts
