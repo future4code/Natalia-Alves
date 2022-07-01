166c04fc6ef74795bb66056cb3217a6461dbfbcc
Natalia Alves <nataliavalins@gmail.com>
2022-06-30 23:22:24 -0300

    testes-e-mocks

---
+++ Modulo7/Testes-e-mocks/jest-template/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ["<rootDir>/tests"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
