d2356a6ba6feab5c1f250f1b5368299f640f6a78
nataliavalins-fga <nataliavalins@gmail.com>
2022-09-08 21:53:11 -0300

    pequena-correcao-versao-final

--- busca-de-servico/doghero/src/routes/WalkingDog.ts
+++ busca-de-servico/doghero/src/routes/WalkingDog.ts
@@ -6,7 +6,7 @@ import { DogWalkingController } from "../controller/DogWalkingController"
 export const WalkingDog = express.Router()
 const walkingController = new DogWalkingController ()
 
-WalkingDog.get('/', walkingController.allwalking)
+WalkingDog.get('/', walkingController.allWalking)
 WalkingDog.post('/walking', walkingController.createWalking)
 WalkingDog.get('/show/:id', walkingController.showWalking)
 WalkingDog.get('/start_walk/:id', walkingController.startWalking)
